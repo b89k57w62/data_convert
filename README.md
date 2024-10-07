a65d2c122fe8662285a24b95900df886f8a1031b
Jeffrey Wu <wu19930628@gmail.com>
2024-10-07 14:01:27 +0800

    completed

---
+++ README.md
@@ -0,0 +1,8 @@
+# Data Convert for SHOP.com
+
+## Usage
+app.py is the entry point to run the program.
+```
+python app.py
+```
+use formal_data.csv to format the raw_data.csv and output the file to outputfile.csv
--- app.py
+++ app.py
@@ -1,27 +1,13 @@
-import csv
-from utils.create_id import IDProcessor
-
-class Pipeline:
-    def __init__(self):
-        self.id_processor = IDProcessor()
-
-    def run(self, input_file, output_file):
-        with open(input_file, 'r', encoding='utf-8') as f:
-            reader = csv.DictReader(f)
-            data = list(reader)
-        
-        processed_data = self.id_processor.process_type_and_ids(data, output_file)
-        
-        with open(output_file, 'w', newline='', encoding='utf-8') as f:
-            writer = csv.DictWriter(f, fieldnames=reader.fieldnames)
-            writer.writeheader()
-            writer.writerows(processed_data)
+from utils.format_header import format_header
+from utils.format_data import format_data
 
 def main():
-    input_file = 'data/input.csv'
-    output_file = 'data/output.csv'
-    pipeline = Pipeline()
-    pipeline.run(input_file, output_file)
+    input_file = 'your_input_file.csv'
+    header_file = 'your_header_file.csv' 
+    output_file = 'your_output_file.csv'
+    format_header(header_file, output_file)
+
+    format_data(input_file, output_file)
 
 if __name__ == "__main__":
     main()
